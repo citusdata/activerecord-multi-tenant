f5a228dab86df619f15ffbb0de4834330d941283
Lukas Fittl <lukas@fittl.com>
2016-09-09 17:10:35 -0700

    Move version-specific gems into their own subdirectories.

--- 4.2/lib/citus-rails-4.2.rb
+++
@@ -1,5 +0,0 @@
-require 'composite_primary_keys'
-
-require 'citus-rails-4.2/acts_as_distributed'
-require 'citus-rails-4.2/belongs_to_association'
-require 'citus-rails-4.2/copy_from_client'
--- 4.2/lib/citus-rails-4.2/belongs_to_association.rb
+++
@@ -1,9 +0,0 @@
-class ActiveRecord::Associations::BelongsToAssociation
-  alias_method :remove_keys_without_cpk, :remove_keys
-  def remove_keys
-    return remove_keys_without_cpk unless reflection.foreign_key.is_a?(Array)
-
-    fk = reflection.foreign_key - [owner.class.partition_column]
-    owner[fk] = nil
-  end
-end
