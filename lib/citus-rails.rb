9871bb058ad0870fa34bb077b47e840beff7bc21
Lukas Fittl <lfittl@shasta.local>
2016-11-17 17:39:06 -0800

    Add migration helpers.

--- lib/citus-rails.rb
+++ lib/citus-rails.rb
@@ -24,6 +24,56 @@ module ActsAsDistributed
     end
   end
 
+  module MigrationExtensions
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+    end
+
+    def create_distributed_table(table_name, partition_key)
+      execute "SELECT create_distributed_table($$#{table_name}$$, $$#{partition_key}$$)"
+    end
+
+    def execute_on_all_nodes(sql)
+      execute sql
+      execute "SELECT citus_run_on_all_workers($$#{sql}$$)"
+    end
+
+    def enable_extension_on_all_nodes(extension)
+      execute_on_all_nodes "CREATE EXTENSION IF NOT EXISTS \"#{extension}\""
+    end
+
+    def enable_citus_tools
+      execute <<-SQL
+      CREATE OR REPLACE FUNCTION citus_run_on_all_workers(command text,
+                            parallel bool default true,
+                            OUT nodename text,
+                            OUT nodeport int,
+                            OUT success bool,
+                            OUT result text)
+        RETURNS SETOF record
+        LANGUAGE plpgsql
+        AS $function$
+      DECLARE
+        workers text[];
+        ports int[];
+        commands text[];
+      BEGIN
+        WITH citus_workers AS (
+          SELECT * FROM master_get_active_worker_nodes() ORDER BY node_name, node_port)
+        SELECT array_agg(node_name), array_agg(node_port), array_agg(command)
+        INTO workers, ports, commands
+        FROM citus_workers;
+
+        RETURN QUERY SELECT * FROM master_run_on_worker(workers, ports, commands, parallel);
+      END;
+      $function$;
+      SQL
+    end
+  end
+
   def self.current_tenant
     ActsAsDistributed.current_tenant
   end
@@ -31,8 +81,16 @@ module ActsAsDistributed
   def self.with(tenant, &block)
     ActsAsTenant.with_tenant(tenant, &block)
   end
+
+  def self.with_id(tenant_id, &block)
+    # TODO
+  end
 end
 
 if defined?(ActiveRecord::Base)
   ActiveRecord::Base.send(:include, ActsAsDistributed::ModelExtensions)
+end
+
+if defined?(ActiveRecord::Migration)
+  ActiveRecord::Migration.send(:include, ActsAsDistributed::MigrationExtensions)
 end
