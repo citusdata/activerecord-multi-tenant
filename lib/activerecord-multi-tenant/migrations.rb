a937a472d590749ed168a242b836afe33dd612ab
Lukas Fittl <lukas@fittl.com>
2017-02-13 14:29:51 -0800

    Migrations: Support single-node Postgres, add support for Citus 6.1.

--- lib/activerecord-multi-tenant/migrations.rb
+++ lib/activerecord-multi-tenant/migrations.rb
@@ -1,16 +1,29 @@
 module MultiTenant
   module MigrationExtensions
     def create_distributed_table(table_name, partition_key)
+      return unless citus_version.present?
       execute "SELECT create_distributed_table($$#{table_name}$$, $$#{partition_key}$$)"
     end
 
     def execute_on_all_nodes(sql)
       execute sql
-      execute "SELECT citus_run_on_all_workers($$#{sql}$$)"
+
+      case citus_version
+      when '6.0'
+        execute "SELECT citus_run_on_all_workers($$#{sql}$$)" # initial citus_tools.sql with different names
+      when nil
+        # Do nothing, this is regular Postgres
+      else # 6.1 and newer
+        execute "SELECT run_command_on_workers($$#{sql}$$)"
+      end
     end
 
     def enable_extension_on_all_nodes(extension)
       execute_on_all_nodes "CREATE EXTENSION IF NOT EXISTS \"#{extension}\""
+    end
+
+    def citus_version
+      execute("SELECT extversion FROM pg_extension WHERE extname = 'citus'").getvalue(0,0).try(:split, '-').try(:first)
     end
   end
 end
