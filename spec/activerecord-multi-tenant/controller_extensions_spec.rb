a8b2a0fe7b0340b2116ec9e587bcba359c5bb948
Lukas Fittl <lukas@fittl.com>
2017-02-13 17:01:13 -0800

    Make API controller additions safe for older Rails versions.

--- spec/activerecord-multi-tenant/controller_extensions_spec.rb
+++ spec/activerecord-multi-tenant/controller_extensions_spec.rb
@@ -16,18 +16,6 @@ class ApplicationController < ActionController::Base
   end
 end
 
-class APIApplicationController < ActionController::API
-  include Rails.application.routes.url_helpers
-  set_current_tenant_through_filter
-  before_action :your_method_that_finds_the_current_tenant
-
-  def your_method_that_finds_the_current_tenant
-    current_account = Account.new
-    current_account.name = 'account1'
-    set_current_tenant(current_account)
-  end
-end
-
 describe ApplicationController, type: :controller do
   controller do
     def index
@@ -41,15 +29,29 @@ describe ApplicationController, type: :controller do
   end
 end
 
-describe APIApplicationController, type: :controller do
-  controller do
-    def index
-      render body: 'custom called'
+if Rails::VERSION::MAJOR >= 5
+  class APIApplicationController < ActionController::API
+    include Rails.application.routes.url_helpers
+    set_current_tenant_through_filter
+    before_action :your_method_that_finds_the_current_tenant
+
+    def your_method_that_finds_the_current_tenant
+      current_account = Account.new
+      current_account.name = 'account1'
+      set_current_tenant(current_account)
     end
   end
 
-  it 'Finds the correct tenant using the filter command' do
-    get :index
-    expect(MultiTenant.current_tenant.name).to eq 'account1'
+  describe APIApplicationController, type: :controller do
+    controller do
+      def index
+        render body: 'custom called'
+      end
+    end
+
+    it 'Finds the correct tenant using the filter command' do
+      get :index
+      expect(MultiTenant.current_tenant.name).to eq 'account1'
+    end
   end
 end
