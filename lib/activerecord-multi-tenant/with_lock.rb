ec66f8e91ca186bc0fc1295f2d617cc13a789d3a
Lukas Fittl <lukas@fittl.com>
2017-02-22 12:41:16 -0800

    Expand with_lock workaround to cover lock! as well.

--- lib/activerecord-multi-tenant/with_lock.rb
+++ lib/activerecord-multi-tenant/with_lock.rb
@@ -2,16 +2,14 @@
 # "SELECT ... FOR UPDATE is not supported for router-plannable queries"
 
 class ActiveRecord::Base
-  alias :with_lock_orig :with_lock
-  def with_lock(&block)
-    if self.class.respond_to?(:scoped_by_tenant?) && MultiTenant.current_tenant_id && MultiTenant.with_lock_workaround_enabled?
-      transaction do
-        self.class.unscoped.where(id: id).update_all(id: id) # No-op UPDATE that locks the row
-        reload # This is just to act similar to the default Rails approach, in case someone relies on the reload
-        yield
-      end
+  alias :lock_orig :lock!
+  def lock!(lock = true)
+    if lock && persisted? && self.class.respond_to?(:scoped_by_tenant?) && MultiTenant.current_tenant_id && MultiTenant.with_lock_workaround_enabled?
+      self.class.unscoped.where(id: id).update_all(id: id) # No-op UPDATE that locks the row
+      reload # This is just to act similar to the default Rails approach, in case someone relies on the reload
+      self
     else
-      with_lock_orig(&block)
+      lock_orig(lock)
     end
   end
 end
