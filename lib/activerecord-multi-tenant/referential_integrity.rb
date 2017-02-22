1a4db9304ce3e6a24be6f943849d74c672a08bd8
Lukas Fittl <lukas@fittl.com>
2017-02-22 12:41:48 -0800

    Enable trigger workaround on Rails 5 as well, otherwise it fails silently.

--- lib/activerecord-multi-tenant/referential_integrity.rb
+++ lib/activerecord-multi-tenant/referential_integrity.rb
@@ -1,15 +1,13 @@
 # Workaround for https://github.com/citusdata/citus/issues/1080
 # "Support DISABLE/ENABLE TRIGGER ALL on distributed tables"
 
-if Rails::VERSION::MAJOR < 5
-  require 'active_record/connection_adapters/postgresql_adapter'
+require 'active_record/connection_adapters/postgresql_adapter'
 
-  module ActiveRecord
-    module ConnectionAdapters
-      class PostgreSQLAdapter < AbstractAdapter
-        def supports_disable_referential_integrity?
-          false
-        end
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter < AbstractAdapter
+      def supports_disable_referential_integrity?
+        false
       end
     end
   end
