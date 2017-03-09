b6ad4470137d196415aa4a590b11fcf0d506c4cc
Nathan Stitt <nathan@stitt.org>
2017-03-08 22:28:42 -0600

    Test version of ActiveRecord instead of Rails
    This allows using library in apps that only use ActiveRecord without the entire Rails stack

--- lib/activerecord-multi-tenant/with_lock.rb
+++ lib/activerecord-multi-tenant/with_lock.rb
@@ -6,7 +6,7 @@ class ActiveRecord::Base
   def lock!(lock = true)
     if lock && persisted? && self.class.respond_to?(:scoped_by_tenant?) && MultiTenant.current_tenant_id && MultiTenant.with_lock_workaround_enabled?
       self.class.unscoped.where(id: id).update_all(id: id) # No-op UPDATE that locks the row
-      reload # This is just to act similar to the default Rails approach, in case someone relies on the reload
+      reload # This is just to act similar to the default ActiveRecord approach, in case someone relies on the reload
       self
     else
       lock_orig(lock)
