fad1c44f37c1781ae90c600cfa9534a262161e6a
Nathan Stitt <nathan@stitt.org>
2017-03-23 13:16:34 -0500

    Test version using ActionPack::VERSION, remove before(:suite)

--- spec/activerecord-multi-tenant/controller_extensions_spec.rb
+++ spec/activerecord-multi-tenant/controller_extensions_spec.rb
@@ -1,65 +1,35 @@
 require 'spec_helper'
 
-describe "ActionController", type: :controller do
-  before(:suite) do
+describe "Controller Extensions", type: :controller do
+  class Account
+    attr_accessor :name
+  end
 
-    class Account
-      attr_accessor :name
+  class ApplicationController < ActionController::Base
+    include Rails.application.routes.url_helpers
+    set_current_tenant_through_filter
+
+    if ActionPack::VERSION::MAJOR < 4
+      before_filter :your_method_that_finds_the_current_tenant
+    else
+      before_action :your_method_that_finds_the_current_tenant
     end
 
-    class ApplicationController < ActionController::Base
-      include Rails.application.routes.url_helpers
-      set_current_tenant_through_filter
-
-      if ActionController::VERSION::MAJOR < 4
-        before_filter :your_method_that_finds_the_current_tenant
-      else
-        before_action :your_method_that_finds_the_current_tenant
-      end
-
-      def your_method_that_finds_the_current_tenant
-        current_account = Account.new
-        current_account.name = 'account1'
-        set_current_tenant(current_account)
-      end
-    end
-
-    describe ApplicationController, type: :controller do
-      controller do
-        def index
-          if ActionController::VERSION::MAJOR >= 5
-            render body: 'custom called'
-          else
-            render text: 'custom called'
-          end
-        end
-      end
-
-      it 'Finds the correct tenant using the filter command' do
-        get :index
-        expect(MultiTenant.current_tenant.name).to eq 'account1'
-      end
-    end
-
-    if ActionController::VERSION::MAJOR >= 5
-      class APIApplicationController < ActionController::API
-        include Rails.application.routes.url_helpers
-        set_current_tenant_through_filter
-        before_action :your_method_that_finds_the_current_tenant
-
-        def your_method_that_finds_the_current_tenant
-          current_account = Account.new
-          current_account.name = 'account1'
-          set_current_tenant(current_account)
-        end
-      end
+    def your_method_that_finds_the_current_tenant
+      current_account = Account.new
+      current_account.name = 'account1'
+      set_current_tenant(current_account)
     end
   end
 
-  describe "APIApplicationController", type: :controller do
+  describe ApplicationController, type: :controller do
     controller do
       def index
-        render body: 'custom called'
+        if ActionPack::VERSION::MAJOR >= 5
+          render body: 'custom called'
+        else
+          render text: 'custom called'
+        end
       end
     end
 
@@ -69,4 +39,30 @@ describe "ActionController", type: :controller do
     end
   end
 
+  if ActionPack::VERSION::MAJOR >= 5
+    class APIApplicationController < ActionController::API
+      include Rails.application.routes.url_helpers
+      set_current_tenant_through_filter
+      before_action :your_method_that_finds_the_current_tenant
+
+      def your_method_that_finds_the_current_tenant
+        current_account = Account.new
+        current_account.name = 'account1'
+        set_current_tenant(current_account)
+      end
+    end
+
+    describe APIApplicationController, type: :controller do
+      controller do
+        def index
+          render body: 'custom called'
+        end
+      end
+
+      it 'Finds the correct tenant using the filter command' do
+        get :index
+        expect(MultiTenant.current_tenant.name).to eq 'account1'
+      end
+    end
+  end
 end
