f835fc10a29c2ab47a51b64fe2ca2117de6adec1
Lukas Fittl <lukas@fittl.com>
2017-05-08 15:04:39 -0700

    Adjust the way the query rewriter is included into activerecord
    Previously there was a chance of failures due to ordering issues
    with Rails autoload mechanism.

--- lib/activerecord-multi-tenant/query_rewriter.rb
+++ lib/activerecord-multi-tenant/query_rewriter.rb
@@ -1,47 +1,47 @@
 require 'active_record'
 
-class TTTenantVisitor < Arel::Visitors::DepthFirst
-  def initialize(arel)
-    super(Proc.new {})
-    @tenant_relations = []
+module MultiTenant
+  class ArelTenantVisitor < Arel::Visitors::DepthFirst
+    def initialize(arel)
+      super(Proc.new {})
+      @tenant_relations = []
 
-    accept(arel.ast)
-  end
+      accept(arel.ast)
+    end
 
-  def tenant_relations
-    @tenant_relations.uniq
-  end
+    def tenant_relations
+      @tenant_relations.uniq
+    end
 
-  def visit_Arel_Table(table, _collector = nil)
-    @tenant_relations << table if tenant_relation?(table)
-  end
+    def visit_Arel_Table(table, _collector = nil)
+      @tenant_relations << table if tenant_relation?(table)
+    end
 
-  def visit_Arel_Nodes_TableAlias(table_alias, _collector = nil)
-    @tenant_relations << table_alias if tenant_relation?(table_alias.left)
-  end
+    def visit_Arel_Nodes_TableAlias(table_alias, _collector = nil)
+      @tenant_relations << table_alias if tenant_relation?(table_alias.left)
+    end
 
-  private
+    private
 
-  def tenant_relation?(table)
-    model = table.name.classify.constantize
-    model && model.respond_to?(:scoped_by_tenant?) && model.scoped_by_tenant?
+    def tenant_relation?(table)
+      model = table.name.classify.constantize
+      model && model.respond_to?(:scoped_by_tenant?) && model.scoped_by_tenant?
+    end
   end
 end
 
-module ActiveRecord
-  module QueryMethods
-    alias :build_arel_orig :build_arel
-    def build_arel
-      arel = build_arel_orig
+class ActiveRecord::Relation
+  alias :build_arel_orig :build_arel
+  def build_arel
+    arel = build_arel_orig
 
-      if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled?
-        relations_needing_tenant_id = TTTenantVisitor.new(arel).tenant_relations
-        arel = relations_needing_tenant_id.reduce(arel) do |arel, relation|
-          arel.where(relation[self.partition_key].eq(MultiTenant.current_tenant_id))
-        end
+    if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled?
+      relations_needing_tenant_id = MultiTenant::ArelTenantVisitor.new(arel).tenant_relations
+      arel = relations_needing_tenant_id.reduce(arel) do |arel, relation|
+        arel.where(relation[self.partition_key].eq(MultiTenant.current_tenant_id))
       end
-
-      arel
     end
+
+    arel
   end
 end
