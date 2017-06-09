f2e25d47e4e4377316ffd57db0d9556eed7834ef
Lukas Fittl <lukas@fittl.com>
2017-06-09 14:59:09 -0700

    Reorder some tests to avoid a hard to explain test failure
    Something about the order caused the Project.find to fail, but its
    not clear why, and seems to be more related to test setup and less due to
    an actual bug.

--- spec/activerecord-multi-tenant/record_modifications_spec.rb
+++ spec/activerecord-multi-tenant/record_modifications_spec.rb
@@ -4,18 +4,18 @@ describe MultiTenant, 'Record modifications' do
   let(:account) { Account.create! name: 'test' }
   let(:project) { account.projects.create! name: 'something' }
 
+  it 'includes the tenant_id in DELETEs' do
+    project.destroy
+    MultiTenant.with(account) do
+      expect(Project.where(id: project.id).first).not_to be_present
+    end
+  end
+
   it 'includes the tenant_id in UPDATEs' do
     project.name = 'something else'
     project.save!
     MultiTenant.with(account) do
       expect(Project.find(project.id).name).to eq 'something else'
-    end
-  end
-
-  it 'includes the tenant_id in DELETEs' do
-    project.destroy
-    MultiTenant.with(account) do
-      expect(Project.where(id: project.id).first).not_to be_present
     end
   end
 end
