deb0dbe56995306495e29116e400559f2156edc8
Lukas Fittl <lukas@fittl.com>
2017-06-13 12:59:47 -0700

    Switch back to Relation-based mechanism of hooking into ActiveRecord
    Sadly the DatabaseStatements adapter based approach does not catch all
    query trees, in particular simple ones. The previous approach worked
    better, although it means that we'll be called multiple times as a query
    is being built.

--- lib/activerecord-multi-tenant/query_rewriter.rb
+++ lib/activerecord-multi-tenant/query_rewriter.rb
@@ -14,38 +14,38 @@ module MultiTenant
     end
 
     def visit_Arel_Table(table, _collector = nil)
-      @tenant_relations << table if tenant_relation?(table)
+      @tenant_relations << table if tenant_relation?(table.table_name)
     end
 
     def visit_Arel_Nodes_TableAlias(table_alias, _collector = nil)
-      @tenant_relations << table_alias if tenant_relation?(table_alias.left)
+      @tenant_relations << table_alias if tenant_relation?(table_alias.table_name)
     end
 
     private
 
-    def tenant_relation?(table)
-      MultiTenant.multi_tenant_model_for_table(table.name).present?
+    def tenant_relation?(table_name)
+      MultiTenant.multi_tenant_model_for_table(table_name).present?
     end
   end
 end
 
+require 'active_record/relation'
 module ActiveRecord
-  module ConnectionAdapters # :nodoc:
-    module DatabaseStatements
-      alias :to_sql_orig :to_sql
-      # Converts an arel AST to SQL
-      def to_sql(arel, binds = [])
-        if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled? &&
-          [Arel::SelectManager, Arel::UpdateManager, Arel::DeleteManager, ActiveRecord::Relation].include?(arel.class)
-          relations_needing_tenant_id = MultiTenant::ArelTenantVisitor.new(arel).tenant_relations
-          arel = relations_needing_tenant_id.reduce(arel) do |arel, relation|
-            model = MultiTenant.multi_tenant_model_for_table(relation.table_name)
-            next arel unless model.present?
-            arel.where(relation[model.partition_key].eq(MultiTenant.current_tenant_id))
-          end
+  module QueryMethods
+    alias :build_arel_orig :build_arel
+    def build_arel
+      arel = build_arel_orig
+
+      if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled?
+        relations_needing_tenant_id = MultiTenant::ArelTenantVisitor.new(arel).tenant_relations
+        arel = relations_needing_tenant_id.reduce(arel) do |arel, relation|
+          model = MultiTenant.multi_tenant_model_for_table(relation.table_name)
+          next arel unless model.present?
+          arel.where(relation[model.partition_key].eq(MultiTenant.current_tenant_id))
         end
-        to_sql_orig(arel, binds)
       end
+
+      arel
     end
   end
 end
