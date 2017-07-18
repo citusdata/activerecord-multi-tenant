37ed5f27568f2f395bc686e45d6f5bd74ee7f7c8
Kyle Bock <kyle.bock@salesloft.com>
2017-07-18 18:50:37 -0400

    Fix Bug preventing FastTruncate from running
    If there was exactly one insert on a table fast truncate would not
    run. This was caused by how postgres sets last_value = start_value when
    that sequence had not been used.

---
+++ spec/activerecord-multi-tenant/fast_truncate_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe MultiTenant::FastTruncate do
+  before(:each) do
+    MultiTenant::FastTruncate.run
+  end
+
+  it "truncates tables that have exactly one row inserted" do
+    Account.create! name: 'foo'
+    expect {
+      MultiTenant::FastTruncate.run
+    }.to change { Account.count }.from(1).to(0)
+  end
+
+  it "truncates tables that have more than one row inserted" do
+    Account.create! name: 'foo'
+    Account.create! name: 'bar'
+
+    expect {
+      MultiTenant::FastTruncate.run
+    }.to change { Account.count }.from(2).to(0)
+  end
+end
