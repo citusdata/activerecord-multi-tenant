b198945282e793d9751cc3ab929303ad47c49f78
Kyle Bock <kyle.bock@salesloft.com>
2017-08-10 20:24:39 -0400

    Improve TenantVisitor
    We modified and added some visit_ methods to the tenant visitor. This
    follows a little bit more closely with arel so that we are operating on
    the ast so that we can better identify where we need to append team ids.

--- lib/activerecord-multi-tenant/query_rewriter.rb
+++ lib/activerecord-multi-tenant/query_rewriter.rb
@@ -1,61 +1,113 @@
 require 'active_record'
 
 module MultiTenant
+  class Table
+    attr_reader :arel_table
+
+    def initialize(arel_table)
+      @arel_table = arel_table
+    end
+
+    def eql?(rhs)
+      self.class == rhs.class &&
+        equality_fields.eql?(rhs.equality_fields)
+    end
+
+    def hash
+      equality_fields.hash
+    end
+
+    protected
+
+    def equality_fields
+      [arel_table.name, arel_table.table_alias]
+    end
+  end
+
+  class Context
+    attr_reader :arel_node, :known_relations, :handled_relations
+
+    def initialize(arel_node)
+      @arel_node = arel_node
+      @known_relations = []
+      @handled_relations = []
+    end
+
+    def discover_relations
+      old_discovering = @discovering
+      @discovering = true
+      yield
+      @discovering = old_discovering
+    end
+
+    def visited_relation(relation)
+      return unless @discovering
+      @known_relations << Table.new(relation)
+    end
+
+    def visited_handled_relation(relation)
+      @handled_relations << Table.new(relation)
+    end
+
+    def unhandled_relations
+      known_relations.uniq - handled_relations
+    end
+  end
+
   class ArelTenantVisitor < Arel::Visitors::DepthFirst
     def initialize(arel)
       super(Proc.new {})
-      @tenant_relations = {}
-      @existing_tenant_relations = {}
-      @joins_by_table_name = {}
       @statement_node_id = nil
 
+      @contexts = []
+      @current_context = nil
       accept(arel.ast)
     end
 
-    def tenant_relations
-      @tenant_relations
+    attr_reader :contexts
+
+    def visit_Arel_Attributes_Attribute(*args)
+      return if @current_context.nil?
+      super(*args)
     end
 
-    def existing_tenant_relations
-      @existing_tenant_relations
-    end
-
-    def joins_by_table_name
-      @joins_by_table_name
-    end
-
-    def visit_Arel_Table(o, _collector = nil)
-      @tenant_relations[@statement_node_id] ||= []
-      @tenant_relations[@statement_node_id] << o if tenant_relation?(o.table_name)
-    end
-    alias :visit_Arel_Nodes_TableAlias :visit_Arel_Table
-
-    def visit_Arel_Nodes_SelectStatement(o, _collector = nil)
-      return if @statement_node_id
-      @statement_node_id = o.object_id
-      visit o.cores
-      visit o.orders
-      visit o.limit
-      visit o.lock
-      visit o.offset
-    end
-
-    def visit_Arel_Nodes_Equality(o, _collector = nil)
+    def visit_Arel_Nodes_Equality(o, *args)
       if o.left.is_a?(Arel::Attributes::Attribute)
         table_name = o.left.relation.table_name
         model = MultiTenant.multi_tenant_model_for_table(table_name)
-        @existing_tenant_relations[@statement_node_id] ||= []
-        @existing_tenant_relations[@statement_node_id] << o.left.relation if model.present? && o.left.name == model.partition_key
+        @current_context.visited_handled_relation(o.left.relation) if model.present? && o.left.name == model.partition_key
+      end
+      super(o, *args)
+    end
+
+    def visit_MultiTenant_TenantEnforcementClause(o, *)
+      @current_context.visited_handled_relation(o.tenant_attribute.relation)
+    end
+
+    def visit_Arel_Table(o, _collector = nil)
+      @current_context.visited_relation(o) if tenant_relation?(o.table_name)
+    end
+    alias :visit_Arel_Nodes_TableAlias :visit_Arel_Table
+
+    def visit_Arel_Nodes_SelectCore(o, *args)
+      nest_context(o) do
+        @current_context.discover_relations do
+          visit o.source
+        end
+        visit o.wheres
+        visit o.groups
+        visit o.windows
+        visit o.having
       end
     end
 
-    def visit_join(o, collector = nil)
-      if o.left.is_a?(Arel::Nodes::TableAlias) || o.left.is_a?(Arel::Table)
-        @joins_by_table_name[@statement_node_id] ||= {}
-        @joins_by_table_name[@statement_node_id][o.left.name] = o
+    def visit_Arel_Nodes_OuterJoin(o, collector = nil)
+      nest_context(o) do
+        @current_context.discover_relations do
+          visit o.left
+          visit o.right
+        end
       end
-      visit o.left
-      visit o.right
     end
     alias :visit_Arel_Nodes_OuterJoin :visit_join
     alias :visit_Arel_Nodes_FullOuterJoin :visit_join
@@ -67,9 +119,32 @@ module MultiTenant
     def tenant_relation?(table_name)
       MultiTenant.multi_tenant_model_for_table(table_name).present?
     end
+
+    DISPATCH = Hash.new do |hash, klass|
+      hash[klass] = "visit_#{(klass.name || '').gsub('::', '_')}"
+    end
+
+    def dispatch
+      DISPATCH
+    end
+
+    def get_dispatch_cache
+      dispatch
+    end
+
+    def nest_context(o)
+      old_context = @current_context
+      @current_context = Context.new(o)
+      @contexts << @current_context
+
+      yield
+
+      @current_context = old_context
+    end
   end
 
   class TenantEnforcementClause < Arel::Nodes::Node
+    attr_reader :tenant_attribute
     def initialize(tenant_attribute)
       @tenant_attribute = tenant_attribute
     end
@@ -116,33 +191,23 @@ module ActiveRecord
 
       if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled?
         visitor = MultiTenant::ArelTenantVisitor.new(arel)
-        visitor.tenant_relations.each do |statement_node_id, relations|
-          # Process every level of the statement separately, so we don't mix subselects
-          known_relations = visitor.existing_tenant_relations[statement_node_id] || []
-          joins_by_table_name = visitor.joins_by_table_name[statement_node_id] || {}
+        visitor.contexts.each do |context|
+          node = context.arel_node
+          context.unhandled_relations.each do |relation|
+            model = MultiTenant.multi_tenant_model_for_table(relation.arel_table.table_name)
+            enforcement_clause = MultiTenant::TenantEnforcementClause.new(relation.arel_table[model.partition_key])
 
-          relations.each do |relation|
-            model = MultiTenant.multi_tenant_model_for_table(relation.table_name)
-            next unless model.present?
-
-            next if known_relations.map(&:name).include?(relation.name)
-            known_relations << relation
-
-            join = joins_by_table_name[relation.name]
-            if join
-              joined_relation = join.right.expr.right.relation
-              joined_relation = join.right.expr.left.relation if joined_relation.table_name == relation.table_name
-              joined_model = MultiTenant.multi_tenant_model_for_table(joined_relation.table_name)
-              tenant_cond = relation[model.partition_key].eq(joined_relation[joined_model.partition_key])
-              join.right.expr = join.right.expr.and(tenant_cond)
-            else
-              ctx = arel.ast.cores.last
-              enforcement_clause = MultiTenant::TenantEnforcementClause.new(relation[model.partition_key])
-              if ctx.wheres.size == 1
-                ctx.wheres = [enforcement_clause.and(ctx.wheres.first)]
+            case node
+            when Arel::Nodes::Join #Arel::Nodes::OuterJoin, Arel::Nodes::RightOuterJoin, Arel::Nodes::FullOuterJoin
+              node.right.expr = node.right.expr.and(enforcement_clause)
+            when Arel::Nodes::SelectCore
+              if node.wheres.empty?
+                node.wheres = [enforcement_clause]
               else
-                arel = arel.where(enforcement_clause)
+                node.wheres[0] = enforcement_clause.and(node.wheres[0])
               end
+            else
+              raise "UnknownContext"
             end
           end
         end
