f0fba7ebe99b1a6928563a4a542f9adbe77a09ac
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-04-11 23:33:37 +0200

    Adapted ModelExtension spec to handle tests with a prepared_statement-on DB config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,4 +42,8 @@ end
 MultiTenantTest::Application.config.secret_token = 'x' * 40
 MultiTenantTest::Application.config.secret_key_base = 'y' * 40
 
+def uses_prepared_statements?
+  ActiveRecord::Base.connection.prepared_statements
+end
+
 require 'schema'
